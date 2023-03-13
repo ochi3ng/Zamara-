2f94914e9ed738e6f3be14f8e153c7f287788e1b
ochi3ng <johnochi3ng@gmail.com>
2023-03-13 14:25:58 +0300

    working on styling

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput,TouchableOpacity, Image} from 'react-native';
 // 192.168.0.114
 export default function App() {
   return (
     <SafeAreaView style={styles.headerContainer}>
-      <View >
-        <Text style={styles.header}>Hello Again!</Text>
-        <Text style={styles.text}>Welcome back you`ve been missed!</Text>
-        <TextInput style={styles.container} placeholder='Enter username'/>
-        <TextInput style={styles.container} placeholder='Password' />
-        <TouchableOpacity>
-          <Text style={styles.button}>Sign In</Text>
-        </TouchableOpacity>
-        <StatusBar style="auto" />
+      <View style={styles.logo}>
+          <Image
+            source={require('./assets/zamaralogo.png')}
+          />
+      </View>
+      <View style={styles.bodyContainer}>
+      
+          <Text style={styles.header}>Hello Again!</Text>
+          <Text style={styles.text}>Welcome back to Zamara</Text>
+         <View style={styles.inputs}>
+          <TextInput style={styles.container} placeholder='Enter username' />
+          <TextInput style={styles.container} placeholder='Password'
+            secureTextEntry={true} />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.signIn}>Sign In</Text>
+          </TouchableOpacity>
+         </View>
+          <StatusBar style="auto" />
+      
       </View>
     </SafeAreaView>
 
@@ -20,12 +30,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  bodyContainer:{
+    backgroundColor:'whitesmoke',
+    width:380,
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
+    height:500
+  },
+
   headerContainer:{
     flex: 1,
-    backgroundColor:
-      'rgb(243, 237, 242)',
+    backgroundColor: 'rgb(31,31,61)',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
  
   header: {
@@ -36,23 +54,39 @@ const styles = StyleSheet.create({
   },
   text:{
     width:300,
+    textAlign:'center',
     fontSize:30
   },
   container: {
     backgroundColor: 'rgb(255,255,255)',
-    marginBottom: 5,
+    marginBottom: 15,
+    width:300,
+    alignItems:'center',
     height:60,
     justifyContent: 'center',
     paddingLeft:10,
     borderRadius:10,
   },
   button:{
-    backgroundColor:'rgb(253,107,104)',
+    backgroundColor:'rgb(31,31,61)',
     height:60,
+    width:300,
+    alignItems:'center',
     textAlign:'center',
     justifyContent:'center',
     borderRadius:11,
-    color:'white',
+    marginTop:50
+  },
+  signIn: {
+   color:'white',
+   textAlign:'center'
+  },
+  logo:{
+    paddingTop:300,
+    marginBottom:20
+  },
+  inputs:{
+  alignItems:'center'
   }
  
 });
